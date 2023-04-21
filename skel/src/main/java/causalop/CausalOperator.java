1d147fd13470ff6d81a8c6fd09b9edd53dbd1871
Yakari144 <yakari144@DESKTOP-MANKC29.localdomain>
2023-04-21 17:49:19 +0100

    testes para tudo o que pode dar certo, para o que pode gerar exceçao, testeOk para 3 processos, testeReorder para 3 processos

--- skel/src/main/java/causalop/CausalOperator.java
+++ skel/src/main/java/causalop/CausalOperator.java
@@ -28,9 +28,12 @@ public class CausalOperator<T> implements ObservableOperator<T, CausalMessage<T>
     // 1 se a mensagem ainda não pode ser entregue
     private int receiveVerify(int[] vm,int j){
         int canDeliver = 0;
-        // se o número de sequência da mensagem recebida é o próximo a ser entregue
+        // se o número de sequência da mensagem recebida ja foi entregue
         if(maxSeqNumbers[j] + 1 > vm[j])
             return -1;
+        // se o número de sequência da mensagem recebida ainda não é o proximo a entregar
+        if(maxSeqNumbers[j] + 1 < vm[j])
+            return 1;
         // verifica se todas as mensagens que era esperado ter recebido de outros processos já foram recebidas
         for (int i = 0; i < n; i++) {
             if (i==j)
@@ -42,6 +45,34 @@ public class CausalOperator<T> implements ObservableOperator<T, CausalMessage<T>
         return canDeliver;
     }
 
+    private CausalMessage<T> checkQueue(){
+        int qS = queue.size();
+        Queue<CausalMessage<T>> fakeq;
+        fakeq = new ArrayDeque<>(queue);
+        for (int i = 0; i < qS; i++) {
+            CausalMessage<T> m = fakeq.poll();
+            int canDeliver = receiveVerify(m.v, m.j);
+            if(canDeliver==0){
+                queue.remove(m);
+                return m;
+            }else if (canDeliver==-1){
+                System.out.println("Mensagem já devia ter sido entregue: " + m.payload);
+                queue.remove(m);
+            }
+        }
+        return null;
+    }
+
+    private String printV(int[] v){
+        String sV = "";
+        for (int i = 0; i < n; i++) {
+            if(i!=0)
+                sV += ",";
+            sV += v[i];
+        }
+        sV += "";
+        return sV;
+    }
 
     @Override
     public @NonNull Observer<? super CausalMessage<T>> apply(@NonNull Observer<? super T> down) throws Throwable {
@@ -49,51 +80,38 @@ public class CausalOperator<T> implements ObservableOperator<T, CausalMessage<T>
 
             @Override
             public void onNext(@NonNull CausalMessage<T> m) {
-                System.out.println("Recebi mensagem " + m.payload + " de " + m.j + " com v = " + m.v[0] + ", " + m.v[1]);
+                String sV = printV(m.v);
+                System.out.println("Recebi mensagem " + m.payload + " de " + m.j + " com v = " + sV);
                 // verifica se a mensagem pode ser entregue
                 int canDeliver = receiveVerify(m.v, m.j);
-
+                
+                // verifica se a mensagem já está na fila de espera
                 if(queue.contains(m)){
                     System.out.println("Mensagem já está na fila de espera: " + m.payload);
                     return;
                 }
-            
-                if (canDeliver==0) {
-                    // mensagem pode ser entregue, passa para baixo e atualiza o vetor de números de sequência
+                
+                // mensagem pode ser entregue
+                if(canDeliver==0){
+                    // passa para baixo 
                     down.onNext(m.payload);
                     System.out.println("Mandei logo a seguir a receber: " + m.payload);
+                    // atualiza o vetor de números de sequência
                     for (int i = 0; i < n; i++) {
-                    //    System.out.println("Antes - maxSeqNumbers[" + i + "] = " + maxSeqNumbers[i]);
                         maxSeqNumbers[i] = Math.max(maxSeqNumbers[i], m.v[i]);
-                    //    System.out.println("Depois - maxSeqNumbers[" + i + "] = " + maxSeqNumbers[i]);
                     }
-                    System.out.println("maxSeqNumbers = " + maxSeqNumbers[0] + ", " + maxSeqNumbers[1]);
+                    sV = printV(maxSeqNumbers);
+                    System.out.println("maxSeqNumbers = " + sV);
                     
-                    // verifica se há mensagens na fila de espera que podem ser entregues agora
-                    int qS = queue.size();
-                    for (int i = 0; i < qS; i++) {
-                        // pega na 1a mensagem na fila de espera
-                        CausalMessage<T> queuedMsg = queue.poll();
-                        int canDeliverQueuedMsg = receiveVerify(queuedMsg.v, queuedMsg.j);
-                        
-                        if (canDeliverQueuedMsg==1) {
-                            System.out.println("Não mandei da Fila de Espera: " + queuedMsg.payload);
+                    // enquanto houver mensagens na fila de espera que possam ser entregues, entrega
+                    while((m = checkQueue())!=null) {
+                        System.out.println("Mandei da fila de espera: " + m.payload);
+                        down.onNext(m.payload);
+                        for (int i = 0; i < n; i++) {
+                            maxSeqNumbers[i] = Math.max(maxSeqNumbers[i], m.v[i]);
                         }
-                    
-                        if (canDeliverQueuedMsg==0) {
-                            // mensagem na fila pode ser entregue, passa para baixo e atualiza o vetor de números de sequência
-                            System.out.println("Mandei da Fila de Espera: " + queuedMsg.payload);
-                            down.onNext(queuedMsg.payload);
-                            for(int j = 0; j < n; j++) {
-                                maxSeqNumbers[j] = Math.max(maxSeqNumbers[j], queuedMsg.v[j]);
-                            }
-                            System.out.println("maxSeqNumbers = " + maxSeqNumbers[0] + ", " + maxSeqNumbers[1]);
-                        } else {
-                            // mensagem na fila não pode ser entregue agora, pare de verificar a fila
-                            // coloca no fim da fila
-                            queue.offer(queuedMsg);
-                            break;
-                        }
+                        sV = printV(maxSeqNumbers);
+                        System.out.println("maxSeqNumbers = " + sV);
                     }
                 } else {
                     // mensagem já devia ter sido entregue
--- skel/src/test/java/causalop/CausalOpTest.java
+++ skel/src/test/java/causalop/CausalOpTest.java
@@ -18,7 +18,19 @@ public class CausalOpTest {
 
         Assert.assertArrayEquals(l.toArray(), new String[]{"a","b","c"});
     }
-    
+
+    @Test
+    public void test3Ok() {
+        var l = Observable.just(
+                        new CausalMessage<String>("a", 2, 0, 0, 1),
+                        new CausalMessage<String>("b", 0, 1, 0, 0),
+                        new CausalMessage<String>("c", 1, 1, 1, 1)
+                )
+                .lift(new CausalOperator<String>(3))
+                .toList().blockingGet();
+
+        Assert.assertArrayEquals(l.toArray(), new String[]{"a","b","c"});
+    }
     
     @Test
     public void testReorder() {
@@ -31,6 +43,22 @@ public class CausalOpTest {
                 .toList().blockingGet();
                 
         Assert.assertArrayEquals(l.toArray(), new String[]{"a","b","c"});
+    }
+
+    @Test
+    public void test3Reorder() {
+        var l = Observable.just(
+                        new CausalMessage<String>("d", 2, 1, 0, 2),
+                        new CausalMessage<String>("b", 2, 0, 1, 1),
+                        new CausalMessage<String>("f", 2, 2, 1, 3),
+                        new CausalMessage<String>("a", 1, 0, 1, 0),
+                        new CausalMessage<String>("e", 0, 2, 0, 2),
+                        new CausalMessage<String>("c", 0, 1, 1, 0)
+                )
+                .lift(new CausalOperator<String>(3))
+                .toList().blockingGet();
+                
+        Assert.assertArrayEquals(l.toArray(), new String[]{"a","b","c","d","e","f"});
     }
     
     @Test
@@ -45,6 +73,7 @@ public class CausalOpTest {
                 .toList().blockingGet();
         Assert.assertArrayEquals(l.toArray(), new String[]{"a","b","c"});
     }
+
     @Test(expected = IllegalArgumentException.class)
     public void testGap() {
         var l = Observable.just(
@@ -55,4 +84,53 @@ public class CausalOpTest {
                 .toList().blockingGet();
     }
     
+    @Test
+    public void testAllOK(){
+        var l = Observable.just(
+        // para testar "adicionar mensagem à fila"
+        new CausalMessage<String>("c", 1, 0 , 2 , 0 ),
+        // para testar "mensagem que ja existe na fila"
+        new CausalMessage<String>("$", 1, 0 , 2 , 0 ),
+        // para testar "mensagem é enviada logo apos ser recebida"
+        new CausalMessage<String>("a", 0, 1 , 0 , 0 ),
+        // para testar "mensagem é enviada logo apos ser recebida"
+        new CausalMessage<String>("b", 1, 0 , 1 , 0 ),
+        // para testar "mensagem é enviada apos enviar mensagem da fila"
+        new CausalMessage<String>("g", 0, 2 , 2 , 3 ),
+        // para testar "mensagem é enviada apos enviar mensagem da fila"
+        new CausalMessage<String>("f", 2, 1 , 2 , 3 ),
+        // para testar "mensagem da fila é enviada"
+        new CausalMessage<String>("e", 2, 1 , 1 , 2 ),
+        // para testar "mensagem da fila ja devia ter sido entregue"
+        new CausalMessage<String>("£", 2, 0 , 2 , 3 ),
+        new CausalMessage<String>("d", 2, 1 , 1 , 1 ),
+        // para testar "mensagem ja devia ter sido entregue"
+        new CausalMessage<String>("§", 2, 0 , 0 , 1 )
+        )
+        .lift(new CausalOperator<String>(3))
+        .toList().blockingGet();
+
+        Assert.assertArrayEquals(l.toArray(), new String[]{"a","b","c","d","e","f","g"});
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testPorEnviar() {
+        var l = Observable.just(
+            new CausalMessage<String>("c", 1, 0 , 2 , 0 ),
+            new CausalMessage<String>("$", 1, 0 , 2 , 0 ),
+            new CausalMessage<String>("a", 0, 1 , 0 , 0 ),
+            new CausalMessage<String>("b", 1, 0 , 1 , 0 ),
+            new CausalMessage<String>("%", 2, 2 , 2 , 5 ),
+            new CausalMessage<String>("g", 0, 2 , 2 , 3 ),
+            new CausalMessage<String>("f", 2, 1 , 2 , 3 ),
+            new CausalMessage<String>("e", 2, 1 , 1 , 2 ),
+            new CausalMessage<String>("£", 2, 0 , 2 , 3 ),
+            new CausalMessage<String>("d", 2, 1 , 1 , 1 )
+            )
+            .lift(new CausalOperator<String>(3))
+            .toList().blockingGet();
+    }
+    
+
 }
